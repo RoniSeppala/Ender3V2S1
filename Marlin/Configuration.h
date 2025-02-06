757ac549d9d2d4ece2daa0eafdb6ef56da5ddabd
Roni Seppälä <roni.o.seppala@gmail.com>
2025-02-06 18:10:25 +0200

    enabled skew correction and added correct values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2388,13 +2388,13 @@
  *    +-------------->X     +-------------->X     +-------------->Y
  *     XY_SKEW_FACTOR        XZ_SKEW_FACTOR        YZ_SKEW_FACTOR
  */
-//#define SKEW_CORRECTION
+#define SKEW_CORRECTION
 
 #if ENABLED(SKEW_CORRECTION)
   // Input all length measurements here:
-  #define XY_DIAG_AC 282.8427124746
-  #define XY_DIAG_BD 282.8427124746
-  #define XY_SIDE_AD 200
+  #define XY_DIAG_AC 99.43
+  #define XY_DIAG_BD 99.8
+  #define XY_SIDE_AD 70.2558
 
   // Or, set the XY skew factor directly:
   //#define XY_SKEW_FACTOR 0.0
